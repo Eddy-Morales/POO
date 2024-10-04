21df04cf9e05c386494a6470ec21dc389218303a
Eddy-Morales <edyis1720@gmail.com>
2024-10-04 11:56:25 +0200

    "realizo los cambios necesarios"

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
 
+
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
+        }
+        String nombre = "Eddy Morales";
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("nombre = " + nombre);
+
         }
     }
 }
